85e13fae2113e540f192f9a12ac9aa7c6effaa99
zhb1nk <zhbink@163.com>
2021-03-14 18:52:30 +0800

    menu模块

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,26 @@
 import React from 'react';
 import Button, { ButtonType, ButtonSize } from './components/Button/button'
+import Menu from './components/Menu/menu'
+import MenuItem from './components/Menu/menuItem'
+import SubMenu from './components/Menu/subMenu'
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Menu>
+          <MenuItem>cool link</MenuItem>
+          <MenuItem>cool link2</MenuItem>
+          <SubMenu title="下拉选项">
+            <MenuItem>
+            <MenuItem>
+              下拉选项二
+            </MenuItem>
+          </SubMenu>
+          <MenuItem>cool link3</MenuItem>
+        </Menu>
+        <hr />
         <Button disabled>disabled</Button>
         <Button btnType={ButtonType.Primary} size={ButtonSize.Large}>primary large</Button>
         <Button btnType={ButtonType.Primary} size={ButtonSize.Small}>primary small</Button>
